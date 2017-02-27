2b51c843598cc4533a9d0a0b6bb1ceeebf7ced75
Ally Sorge <asorge@andrew.cmu.edu>
2017-02-27 04:36:14 -0500

    adding js

---
+++ index.js
@@ -0,0 +1,5 @@
+  <div id="graphic"></div>
+  <script type="text/javascript" src="http://assets.motherjones.com/interactives/plugins/pym.js/src/pym.js"></script>
+  <script>
+        var pymParent = new pym.Parent('graphic', 'distribution URL here', {});
+  </script>
